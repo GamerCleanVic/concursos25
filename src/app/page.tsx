133a5d782e4ca10d79ac1763fb7b649416d1b11a
GamerCleanVic <jonassd2000@gmail.com>
2025-07-16 21:00:54 -0500

    stage 2

---
+++ src/app/page.tsx
@@ -0,0 +1,188 @@
+//import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+
+      <div className="text-4xl font-bold">CONCURSOS</div>
+      <div className="text-3xl">UMA AULA DE CADA CONSECUTIVAMENTE</div>
+      <div className="text-2xl mt-3">MATÉRIAS</div>
+      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left border-solid border-4 border-blue-800 rounded hover:border-pink-700 duration-700 p-3 mt-1">
+        <a
+          href="https://www.youtube.com/playlist?list=PL90gDMfBdI0FjOmIkW_B8UEwy4lW4wz0Z"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 card-buttom"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+            Matemática Concursos{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+          Ivanir Grings - AlfaCon.
+          </p>
+        </a>
+
+        <a
+          href="https://www.youtube.com/playlist?list=PL90gDMfBdI0GETosGEYUhUzCu1A7l22MB"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 card-buttom"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+            Redação Concursos{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+          Heitor Ferreira - AlfaCon.
+          </p>
+        </a>
+
+        <a
+          href="https://www.youtube.com/playlist?list=PL90gDMfBdI0EBnfbi8ngHbG6MDPXf2LUB"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 card-buttom"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+            Raciocínio Lógico Concursos{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+          Daniel Lustosa - AlfaCon.
+          </p>
+        </a>
+
+        <a
+          href="https://www.youtube.com/playlist?list=PL90gDMfBdI0EzkdIVKT1pmku9Fo9qvOjL"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 card-buttom"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+            Português Concursos{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+          Giancarla Bombonato - AlfaCon.
+          </p>
+        </a>
+
+        <a
+          href="https://www.youtube.com/playlist?list=PL90gDMfBdI0HQSqDetqazIBstbB85dVca"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 card-buttom"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+            Informática Concursos{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+          João Paulo - AlfaCon.
+          </p>
+        </a>
+
+        <a
+          href="https://www.youtube.com/playlist?list=PL90gDMfBdI0EKwcClb0LNg0Lo9zxHqe_1"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 card-buttom"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+            Dir. Constitucional Concursos{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+          Thais Zanatta - AlfaCon.
+          </p>
+        </a>
+
+        <a
+          href="https://www.youtube.com/playlist?list=PL90gDMfBdI0Gjb1BLcYU3zbAnQIMQSMGm"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 card-buttom"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+            Dir. Administrativo - Concursos{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+          Tiago Vidal - AlfaCon.
+          </p>
+        </a>
+
+        </div>
+
+        <div className="text-2xl mt-3">EXTRAS</div>
+      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left border-solid border-4 border-blue-800 rounded hover:border-pink-700 duration-700 p-3 mt-1">
+        <a
+          href="https://www.youtube.com/playlist?list=PL90gDMfBdI0F1psUyTDbPTYOtmRHkC818"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 card-buttom"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+            Dir. Penal - Concursos{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+          Filipe Ávila - AlfaCon.
+          </p>
+        </a>
+
+        <a
+          href="https://www.youtube.com/playlist?list=PL90gDMfBdI0E5kJl3cSGRynlQNzMmM7Yy"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 card-buttom"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+            Ética OAB - Concursos{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+          Estratégia, Capriata & AlfaCon.
+          </p>
+        </a>
+
+        <a
+          href="https://www.youtube.com/playlist?list=PL90gDMfBdI0HQGRY0v5Um4QuHOu-KWCE7"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 card-buttom"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+            Direito Tributário I - Concursos{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+          Estratégia Concursos.
+          </p>
+        </a>
+
+        </div>
+    </main>
+  );
+}
